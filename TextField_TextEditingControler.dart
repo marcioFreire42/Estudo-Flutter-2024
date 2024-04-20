79e2d95730d3d9fb820aa39e659fe46a13942960
marcioFreire42 <c135532@gmail.com>
2024-04-20 12:01:25 -0300

    Adicionado TextField com controler, aceitando apenas números e só quatro digitos!

--- TextField_TextEditingControler.dart
+++ TextField_TextEditingControler.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,20 +31,50 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Teste do TextField!'),
+          title: Text('Teste do TextField !!!'),
         ),
         body: TesteDoTextField());
   }
 }
 
-class TesteDoTextField extends StatelessWidget {
+class TesteDoTextField extends StatefulWidget {
   TesteDoTextField({super.key});
 
+  @override
+  State<TesteDoTextField> createState() => _TesteDoTextFieldState();
+}
+
+class _TesteDoTextFieldState extends State<TesteDoTextField> {
   // Objeto p/ receber texto de Campo 1 pelo onChanged
   late String textoDigitado;
 
   // Objeto para receber o texto do campo 2;
   TextEditingController controlerTexto2 = TextEditingController();
+  TextEditingController controlerTexto3 = TextEditingController();
+
+  // Vamos usar o initState para adicionar uma alteração a um widget, no caso:
+  // vamos controlar para que o campo 3 de TextField Só receba 4 digitos
+  @override
+  void initState() {
+    super.initState(); // Essa linha deve ser chamada em método @override;
+    controlerTexto3.addListener(() {
+      // Adicionamos um listener para o controler3
+      final texto = controlerTexto3.text;
+      if (texto.length > 4) {
+        controlerTexto3.text = texto.substring(0, 4);
+        // Esse IF analisa se o texto do controler é maior do que 4;
+        // Se for, o substring vai excluir os digitos excedentes;
+      }
+    });
+  }
+
+  // Usado para evitar vazamento de memória,
+  // Esse método libera o controler3 e finaliza o widget corretamente;
+  @override
+  void dispose() {
+    super.dispose();
+    controlerTexto3.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -81,7 +117,29 @@ class TesteDoTextField extends StatelessWidget {
               onPressed: () {
                 print('${controlerTexto2.text}');
               },
-              child: Text('confirma!'))
+              child: Text('confirma!')),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: TextField(
+              // Vamos implementar mais opções nesse campo
+              // Esse campo só receberá digitos e só 4.
+              // O tamanho de 4 digitos foi feito pelo initState.
+              controller: controlerTexto3,
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter.allow(RegExp('[0-9]'))
+                // Aqui filtramos para que somente os digitos de 0 a 9 possam ser digitados;
+                // A sugestão era dentro do RegExp usar '[0-9], {0,4}', mas do jeito que está deu certo.
+              ],
+              decoration: const InputDecoration(
+                  labelText: 'Digite o texto 3!', border: OutlineInputBorder()),
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                print('${controlerTexto3.text}');
+              },
+              child: Text('confirma!!!')),
         ],
       ),
     );
